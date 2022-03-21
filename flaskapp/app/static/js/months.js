006d94d5d87c2bb8d7dfb5fb140dec631d7f6ede
Marius Roets <roets.marius@gmail.com>
2022-03-21 16:05:22 +0200

    Make months clickable

--- flaskapp/app/static/js/months.js
+++ flaskapp/app/static/js/months.js
@@ -3,8 +3,16 @@ $(document).ready(function() {
     var table = new Tabulator("#table", {
         height: 600,
         columns: [
-            {title: "Month", field: "effective_month"},
+            {
+                title: "Month",
+                field: "effective_month",
+            },
         ],
+        rowClick: function(e, row) {
+            var url = "/month/" + row.getData().effective_month;
+            console.log(url);
+            window.location = url;
+        },
     });
     $.get("/api/months", function(data) {
         table.setData(data);
